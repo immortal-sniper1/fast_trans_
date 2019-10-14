748c6bd7ad0b2444cea0ef69bc36cbff27f328b7
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2019-10-14 08:32:36 +0300

    Initial commit

---
+++ prototypes/item.lua
@@ -0,0 +1,130 @@
+data:extend({
+
+ {
+    type = "item-with-entity-data",
+    name = "fast-one",
+    
+    icon = "__fast_trans__/graphics/icon.png",
+	icon_size = 32,
+    subgroup = "transport",
+    place_result="fast-one",
+    stack_size = 15,
+  },
+
+   {
+    type = "item-with-entity-data",
+    name = "fast-one-mk2",
+    localised_name = {"item-name.fast-one-mk2"},
+    icon = "__fast_trans__/graphics/icon2.png",
+	icon_size = 32,
+    subgroup = "transport",
+    place_result="fast-one-mk2",
+    stack_size = 15,
+  },
+
+     {
+    type = "item-with-entity-data",
+    name = "fast-one-mk3",
+    localised_name = {"item-name.fast-one-mk3"},
+    icon = "__fast_trans__/graphics/icon3.png",
+    icon_size = 32,
+    subgroup = "transport",
+    place_result="fast-one-mk3",
+    stack_size = 15,
+  },
+
+
+ {
+    type = "item",
+    name = "giga-fuel",
+    fuel_value = "3GJ",
+    fuel_acceleration_multiplier = 2.9,
+    fuel_category = "giga-fuel",
+    fuel_top_speed_multiplier = 2.3,
+    icon = "__fast_trans__/graphics/coke.png",
+    icon_size = 32,
+    subgroup = "energy",
+    --order = "giga-fuel",
+    stack_size = 10
+},
+
+
+  {
+    type = "item-with-entity-data",
+    name = "cargo-wagon-immortal-mk2",
+    icon = "__base__/graphics/icons/cargo-wagon.png",
+    icon_size = 32,
+    subgroup = "transport",
+    order = "a[train-system]-g[cargo-wagon]",
+    place_result = "cargo-wagon-immortal-mk2",
+    stack_size = 15
+  },
+  {
+    type = "item-with-entity-data",
+    name = "fluid-wagon-immortal-mk2",
+    icon = "__base__/graphics/icons/fluid-wagon.png",
+    icon_size = 32,
+    subgroup = "transport",
+    order = "a[train-system]-h[fluid-wagon]",
+    place_result = "fluid-wagon-immortal-mk2",
+    stack_size = 15
+  },
+
+  {
+    type = "item-with-entity-data",
+    name = "cargo-wagon-immortal-mk3",
+    icon = "__base__/graphics/icons/cargo-wagon.png",
+    icon_size = 32,
+    subgroup = "transport",
+    order = "a[train-system]-g[cargo-wagon]",
+    place_result = "cargo-wagon-immortal-mk3",
+    stack_size = 15
+  },
+  
+  {
+    type = "item-with-entity-data",
+    name = "fluid-wagon-immortal-mk3",
+    icon = "__base__/graphics/icons/fluid-wagon.png",
+    icon_size = 32,
+    subgroup = "transport",
+    order = "a[train-system]-h[fluid-wagon]",
+    place_result = "fluid-wagon-immortal-mk3",
+    stack_size = 15
+  },
+
+
+ {
+    type = "item",
+    name = "giga-fuel-mk2",
+    fuel_value = "7GJ",
+    fuel_acceleration_multiplier = 3.75,
+    fuel_category = "giga-fuel",
+    fuel_top_speed_multiplier = 3.5,
+    icon = "__fast_trans__/graphics/coke2.png",
+    icon_size = 32,
+    subgroup = "energy",
+    --order = "giga-fuel",
+    stack_size = 5
+},
+
+ {
+    type = "item",
+    name = "giga-fuel-mk3",
+    fuel_value = "55GJ",
+    fuel_acceleration_multiplier = 5.9,
+    fuel_category = "giga-fuel",
+    fuel_top_speed_multiplier = 5.95,
+    icon = "__fast_trans__/graphics/coke3.png",
+    icon_size = 32,
+    subgroup = "energy",
+    --order = "giga-fuel",
+    stack_size = 1
+},
+
+
+
+
+
+
+})
+
---
+++ prototypes/recipe.lua
@@ -0,0 +1,225 @@
+data:extend({
+ {
+    type = "recipe",
+    name = "fast-one-ceva text aici",
+    enabled = "false",
+    ingredients = 
+    {
+      {"locomotive",2},
+      {"steel-plate",200},
+      {"iron-plate",500},
+    },
+    result = "fast-one"
+  },
+
+
+ {
+    type = "recipe",
+    name = "fast-one-mk2 txt",
+    enabled = "false",
+    ingredients = 
+    {
+      {"fast-one",3},
+      {"steel-plate",500},
+      {"iron-plate",500},
+      {"electronic-circuit",200},
+
+    },
+    result = "fast-one-mk2"
+  },
+
+   {
+    type = "recipe",
+    name = "fast-one-mk3 txt",
+    enabled = "false",
+    ingredients = 
+    {
+      {"fast-one-mk2",5},
+      {"steel-plate",1000},
+      {"iron-plate",1000},
+      {"electronic-circuit",700},
+
+    },
+    result = "fast-one-mk3"
+  },
+
+
+
+
+
+
+
+
+    {
+        type = "fuel-category",
+        name = "giga-fuel"
+    },
+
+
+     {
+    type = "recipe",
+    name = "giga-fuel",
+    category = "crafting-with-fluid",
+    enabled = "true",
+    ingredients =
+    {
+      {type="fluid", name="light-oil", amount=300},
+      {"coal",375}
+    },
+    result = "giga-fuel"
+  },
+       {
+    type = "recipe",
+    name = "giga-fuel2",
+    category = "crafting-with-fluid",
+    enabled = "true",
+    ingredients =
+    {
+      {type="fluid", name="heavy-oil", amount=300},
+      {"coal",350}
+    },
+    result = "giga-fuel"
+  },
+
+
+{
+    type = "recipe",
+    name = "giga-fuel3",
+
+    enabled = "true",
+    ingredients =
+    {
+            {"solid-fuel",125}
+
+     },
+    result = "giga-fuel"
+  },
+
+  
+
+  {
+    type = "recipe",
+    name = "giga-fuel-mk2-2",
+
+    enabled = "true",
+    ingredients =
+    {
+            {"giga-fuel",25}
+
+     },
+    results = { { name = "giga-fuel-mk2", amount = 10 }
+    }
+  },
+
+
+  {
+    type = "recipe",
+    name = "giga-fuel-mk3",
+
+    enabled = "true",
+    ingredients =
+    {
+            {"giga-fuel-mk2",16},
+            {"uranium-fuel-cell",16}
+
+     },
+    results = { { name = "giga-fuel-mk3", amount = 4 }
+    }
+  },
+
+
+
+
+
+
+
+
+----wagons
+
+
+
+
+  {
+    type = "recipe",
+    name = "cargo-wagon-immortal-mk2",
+    energy_required = 1,
+    enabled = false,
+    ingredients =
+    {
+      {"iron-gear-wheel", 10},
+      {"cargo-wagon", 3},
+      {"iron-plate", 20},
+      {"steel-plate", 20}
+    },
+    result = "cargo-wagon-immortal-mk2"
+  },
+  {
+    type = "recipe",
+    name = "fluid-wagon-immortal-mk2",
+    enabled = false,
+    energy_required = 1.5,
+    ingredients =
+    {
+      {"iron-gear-wheel", 10},
+      {"fluid-wagon", 3},
+      {"steel-plate", 16},
+      {"storage-tank", 2}
+    },
+    result = "fluid-wagon-immortal-mk2"
+  },
+
+ {
+    type = "recipe",
+    name = "cargo-wagon-immortal-mk3",
+    energy_required = 1,
+    enabled = false,
+    ingredients =
+    {
+      {"iron-gear-wheel", 10},
+      {"cargo-wagon-immortal-mk2", 3},
+      {"iron-plate", 20},
+      {"steel-plate", 20}
+    },
+    result = "cargo-wagon-immortal-mk3"
+  },
+  {
+    type = "recipe",
+    name = "fluid-wagon-immortal-mk3",
+    enabled = false,
+    energy_required = 1.5,
+    ingredients =
+    {
+      {"iron-gear-wheel", 10},
+      {"fluid-wagon-immortal-mk2", 3},
+      {"steel-plate", 16},
+      {"storage-tank", 2}
+    },
+    result = "fluid-wagon-immortal-mk3"
+  },
+
+
+
+
+
+
+
+
+
+
+
+})
+
+normall={"giga-fuel", "giga-fuel2","giga-fuel3","giga-fuel-mk2-2","giga-fuel-mk3"}
+
+for _, module in pairs(data.raw.module) do
+  if module.effect and module.limitation then
+    for effect_name in pairs(module.effect) do
+      if effect_name == "productivity"then
+    for _, item in pairs( normall) do
+                table.insert(module.limitation,    item)
+    end
+        break
+      end
+    end
+  end
+end
